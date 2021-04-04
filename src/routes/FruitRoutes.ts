c882513bb3107ac596b7475fe60dc6d4606e3577
murilo9 <murilohenriquematias@gmail.com>
2021-04-04 16:24:25 -0300

    Request exception handling

--- src/routes/FruitRoutes.ts
+++ src/routes/FruitRoutes.ts
@@ -7,23 +7,35 @@ export default class FruitRoutes {
   public routes(app): void {
 
     app.get('/fruits', async (req: Request, res: Response) => {
-      const fruitRepo = getRepository(Fruit)
-      const fruits = await fruitRepo.find()
-      res.status(200).send(fruits)
+      try{
+        const fruitRepo = getRepository(Fruit)
+        const fruits = await fruitRepo.find()
+        res.status(200).send(fruits)
+      }
+      catch(e){
+        console.log(e)
+        res.status(400).send(e)
+      }
     })
 
     app.post('/fruits', async (req: Request, res: Response) => {
       console.log(req.body)
-      const name = (req.body.name || 'noname') as string
-      const color = req.body.color as string
-      const weight = req.body.weight as number
-      const fruitRepo = getRepository('Fruit')
-      const brandNewFruit = new Fruit()
-      brandNewFruit.name = name
-      brandNewFruit.color = color
-      brandNewFruit.weight = weight
-      await fruitRepo.save(brandNewFruit)
-      res.status(200).send(brandNewFruit)
+      try{
+        const name = (req.body.name || 'noname') as string
+        const color = req.body.color as string
+        const weight = req.body.weight as number
+        const fruitRepo = getRepository('Fruit')
+        const brandNewFruit = new Fruit()
+        brandNewFruit.name = name
+        brandNewFruit.color = color
+        brandNewFruit.weight = weight
+        await fruitRepo.save(brandNewFruit)
+        res.status(200).send(brandNewFruit)
+      }
+      catch(e){
+        console.log(e)
+        res.status(400).send(e)
+      }
     })
   }
 }
